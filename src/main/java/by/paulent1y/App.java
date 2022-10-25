5d19d519bf5af93350ecbe6a50979909983040a1
paulent1y <gispavel2017@gmail.com>
2022-10-25 23:56:55 +0200

    initial

---
+++ src/main/java/by/paulent1y/App.java
@@ -0,0 +1,13 @@
+package by.paulent1y;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
